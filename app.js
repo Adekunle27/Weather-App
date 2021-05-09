638a8535d296efbf724fe465e57da23fec5a92bf
Adekunle27 <malikabdulazeez27@gmail.com>
2021-05-09 12:27:28 +0530

    Error Handling using callback 'error' (object) parameter

--- app.js
+++ app.js
@@ -19,10 +19,13 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCvZS9g-QDJZobVSQMJzDSk1V2CzMT6U4c`,
     json: true
 },(error, response, body) => {
-    // console.log(JSON.stringify(response, undefined, 2));
-    // console.log(JSON.stringify(error, undefined, 2));
-    // console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Address : ${body.results[0].formatted_address}`);
-    console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+    if(error){
+        console.log('Unable to connect to google server.');
+    }else if(body.status === 'ZERO_RESULTS'){
+        console.log('Unable to find the address.');
+    }else if(body.status === 'OK'){
+        console.log(`Address : ${body.results[0].formatted_address}`);
+        console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+    }
 })
