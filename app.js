2ee919a660f99a9466cff63a33dddc46e629a54d
Adekunle27 <malikabdulazeez27@gmail.com>
2021-05-10 13:48:31 +0530

    Abstracting Callbacks

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const request  = require('request');
-
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const yrg = yargs
     .options({
@@ -13,19 +13,10 @@ const yrg = yargs
     })
     .argv;
 
-var encodedAddress = encodeURIComponent(yrg.a);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCvZS9g-QDJZobVSQMJzDSk1V2CzMT6U4c`,
-    json: true
-},(error, response, body) => {
-    if(error){
-        console.log('Unable to connect to google server.');
-    }else if(body.status === 'ZERO_RESULTS'){
-        console.log('Unable to find the address.');
-    }else if(body.status === 'OK'){
-        console.log(`Address : ${body.results[0].formatted_address}`);
-        console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(yrg.a, (errorMessage, results)=> {
+    if(errorMessage){
+        console.log(errorMessage);
+    }else{
+        console.log(JSON.stringify(results, undefined, 2));
     }
-})
+});
